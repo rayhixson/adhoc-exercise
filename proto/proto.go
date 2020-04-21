2921728bdb1ee1639b9822669551572851951425
Ray <rhixson@treetopllc.com>
2020-04-21 10:11:53 -0700

    proto seems to be working

--- proto/proto.go
+++ proto/proto.go
@@ -10,6 +10,7 @@ import (
 
 const logFileName = "txnlog.dat"
 const MagicValue = "MPS7"
+const SpecialUser = uint64(2456938384156277127)
 
 type RecordType byte
 
@@ -32,6 +33,15 @@ type Record struct {
 	UserID    [8]byte
 }
 
+type Sum struct {
+	UserID        uint64
+	Credits       float64
+	Debits        float64
+	AutopayStarts int64
+	AutopayEnds   int64
+	UserBalance   float64
+}
+
 func main() {
 	logFile, err := os.Open(logFileName)
 	if err != nil {
@@ -49,37 +59,72 @@ func main() {
 		log.Panic("Bad Magic")
 	}
 
-	fmt.Printf("%v, %v, %v\n", string(h.Magic[:]),
+	recCount := binary.BigEndian.Uint32(h.RecordCount[:])
+	fmt.Printf("Header: %v, %v, %v\n", string(h.Magic[:]),
 		h.Version,
-		binary.BigEndian.Uint32(h.RecordCount[:]))
+		recCount)
 
-	rec, amount, err := ReadRecord(logFile)
-	if err != nil {
-		log.Panic("Bad read", err)
+	sum := Sum{UserID: SpecialUser}
+	for i := uint32(0); i < recCount; i++ {
+		err := ReadRecord(logFile, &sum)
+		if err != nil {
+			log.Panic("Bad read", err)
+		}
 	}
 
-	fmt.Printf("%v, %v, %v, %v\n",
-		rec.Type,
-		binary.BigEndian.Uint32(rec.Timestamp[:]),
-		binary.BigEndian.Uint64(rec.UserID[:]),
-		amount)
+	fmt.Printf("total credit amount=%.2f\ntotal debit amount=%.2f\nautopays started=%d\nautopays ended=%d\nbalance for user %d=%.2f",
+		sum.Credits,
+		sum.Debits,
+		sum.AutopayStarts,
+		sum.AutopayEnds,
+		sum.UserID,
+		sum.UserBalance)
 }
 
-func ReadRecord(buf io.Reader) (rec Record, amount float64, err error) {
+func ReadRecord(buf io.Reader, sum *Sum) (err error) {
+	rec := Record{}
 	err = binary.Read(buf, binary.BigEndian, &rec)
 
 	if err != nil {
-		return rec, 0, err
+		return err
 	}
+
+	var amount float64
 
 	switch rec.Type {
 	case Debit:
 		binary.Read(buf, binary.BigEndian, &amount)
-		return rec, (-1 * amount), nil
+		sum.Debits += amount
+
+		if binary.BigEndian.Uint64(rec.UserID[:]) == SpecialUser {
+			sum.UserBalance -= amount
+		}
+
 	case Credit:
 		binary.Read(buf, binary.BigEndian, &amount)
-		return rec, amount, nil
+		sum.Credits += amount
+
+		if binary.BigEndian.Uint64(rec.UserID[:]) == SpecialUser {
+			sum.UserBalance += amount
+		}
+
+		fmt.Println(amount)
+	case StartAutopay:
+		sum.AutopayStarts++
+
+	case EndAutopay:
+		sum.AutopayEnds++
+	default:
+		return fmt.Errorf("Unknown tran type: %v", rec.Type)
 	}
 
-	return rec, 0, nil
+	/*
+		fmt.Printf("%v, %v, %v, %v\n",
+			rec.Type,
+			binary.BigEndian.Uint32(rec.Timestamp[:]),
+			binary.BigEndian.Uint64(rec.UserID[:]),
+			amount)
+	*/
+
+	return nil
 }
